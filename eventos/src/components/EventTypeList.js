c379005df39329980755e39dd64e76088e818c0d
UnaiGMorchon <unaitxu79@gmail.com>
2023-05-03 11:11:57 +0200

    idiomas de los eventype

--- eventos/src/components/EventTypeList.js
+++ eventos/src/components/EventTypeList.js
@@ -1,8 +1,12 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useContext} from "react";
+import { LanguageContext } from "../App";
+
 
 
 const EventTypeList = ({handleClick, selectedType}) =>{
     const[eventTypes, setEventTypes] = useState([]); // el array vacio es el valor por defecto del eventTypes
+    const language =useContext(LanguageContext);
+
 
         useEffect( () => { // esta funcion se llama cuando hay un cambio del estado  y solo se ejecuta una vez al principio
             fetch ("https://api.euskadi.eus/culture/events/v1.0/eventType") // lalama d todos los tipos de eventos
@@ -12,17 +16,29 @@ const EventTypeList = ({handleClick, selectedType}) =>{
             })
         },[]); // tiene q ser un array vacio para que no falle. El array al estar vacio solo se ejecuta una vez.
 
+
+const allName ={
+    "eu" : "Guztiak",
+    "es" : "Todos"
+};
+
+
         return (
             <div>
                 <h4>Tipos de eventos</h4>
                 <ul className="eventbutton">
-                        <li className={selectedType=== 0 ? "selected btn" : "btn"} onClick={()=> handleClick(0)}>Todos</li>
-                    {eventTypes.map (eventType => ( // montramos datos de id de name es dos idiomas en una lista 
-                        <li className={selectedType=== eventType.id ? "selected btn" : "btn"} key={eventType.id} onClick={()=> handleClick(eventType.id)}>
-                            <img className="icons" src={`/img/${eventType.nameEs.toLowerCase()}.png`} alt={eventType.nameEs} />
-                            {eventType.nameEs} {/* / {eventType.nameEu}  creamos un elememto de lista*/}
+                        <li className={selectedType=== 0 ? "selected btn" : "btn"} onClick={()=> handleClick(0)}>{allName[language]}</li>
+                    {eventTypes.map (eventType => {    // montramos datos de id de name es dos idiomas en una lista 
+                        const name ={
+                            "eu" : eventType.nameEu,
+                            "es" : eventType.nameEs
+                        };
+                    
+                        return <li className={selectedType=== eventType.id ? "selected btn" : "btn"} key={eventType.id} onClick={()=> handleClick(eventType.id)}>
+                            <img className="icons" src={`/img/${eventType.nameEs.toLowerCase()}.png`} alt={name[language]} />
+                            {name[language]} {/* / {eventType.nameEu}  creamos un elememto de lista*/}
                         </li>
-                    ))}
+                    })}
                 </ul>
             </div>
         )
